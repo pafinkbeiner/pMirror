f72a26a21f3f4cd9907fd8ebef5842cc822445f0
pafinkbeiner <paul-d-f@web.de>
2021-02-28 22:01:50 +0100

    Interval

--- mirror/src/Helper/Time.js
+++ mirror/src/Helper/Time.js
@@ -8,7 +8,7 @@ export const useDate = () => {
         const timer = setInterval(() => { // Creates an interval which will update the current data every minute
         // This will trigger a rerender every component that uses the useDate hook.
         setDate(new Date());
-      }, 60 * 1000);
+      }, 10 * 1000);
       return () => {
         clearInterval(timer); // Return a funtion to clear the timer so that it will stop being called on unmount
       }
